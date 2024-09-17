608c67a4e85b062c63e59690e38bae7f3c746bcc
Thomas Eg <164910727+ThomasEgMatthiesen@users.noreply.github.com>
2024-09-17 11:54:01 +0200

    values

--- scripts/values-types-and-operators.js
+++ scripts/values-types-and-operators.js
@@ -1,4 +1,8 @@
 
+////////////////
+////  Bits  ////
+////////////////
+
 // Easy toString method
 function toBinaryString(quotient) {
     let binary = quotient.toString(2); // Convert to binary using base 2
@@ -16,10 +20,10 @@ function toBinaryAlgorithmic(quotient) {
     return binary; // Output binary
 }
 
-// Bitwise operation
+// Bitwise operation, somehow doesn't work with 64-bit...
 function toBinaryBitwise(quotient) {
     let binary = '';
-    for (let i = 7; i >= 0; i--) { // 8-bit
+    for (let i = 63; i >= 0; i--) { // 8-bit, JS uses 64-bit
         let bit = (quotient >> i) & 1; // Shift and mask
         binary += bit;
     }
@@ -28,4 +32,29 @@ function toBinaryBitwise(quotient) {
 
 // console.log('toString method: ' + toBinaryString(97));
 // console.log('Algorithmic: ' + toBinaryAlgorithmic(97));
-console.log('Bitwise: ' + toBinaryBitwise(97));
+// console.log('Bitwise: ' + toBinaryBitwise(97));
+
+
+//////////////////
+/// Arithmetic ///
+//////////////////
+
+// +, -, *, /, %,
+
+/////////////////
+///  Strings  ///
+/////////////////
+
+var backticks = `Down on the sea`;
+var doubleQuotes = "Lie on the ocean";
+var quotes = 'Float on the ocean';
+
+// Line breaks
+var linesQuotes = "This is the first line\nAnd this is the second";
+var linesBackticks = `This 
+    is the      first       line
+And this
+        is the           second`;
+
+// console.log(linesQuotes);
+// console.log(linesBackticks);
