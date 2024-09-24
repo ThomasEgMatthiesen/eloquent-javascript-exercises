a253c12827906dabacde9dbde623478e33bc45fa
Thomas Eg <164910727+ThomasEgMatthiesen@users.noreply.github.com>
2024-09-24 18:35:39 +0200

    Chapter 2

---
+++ scripts/program-structure.js
@@ -0,0 +1,147 @@
+var hStyle = 'background-color: black; color: white; font-weight: bold; padding: 4px 8px';
+
+// Looping a triangle
+console.log('%c Looping a triangle', hStyle);
+console.log(`Write a loop that makes seven calls to console.log to output the following triangle:`)
+
+for (let hash = "#"; hash.length <= 7; hash += "#") {
+    console.log(hash);
+}
+
+// FizzBuzz
+console.log('%c FizzBuzz', hStyle);
+console.log(`
+Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. 
+
+For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.
+
+`)
+
+let numbers = [];
+
+for (let num = 1; num <= 100; num++) {
+    if (num % 3 === 0) {
+        numbers.push('fizz');
+    } else if (num % 5 === 0) {
+        numbers.push('buzz');
+    } else {
+        numbers.push(num);
+    }
+}
+
+console.log(`
+Logged as an array for readability: 
+
+${numbers}
+
+`);
+
+console.log(`
+When you have that working, modify your program to print "FizzBuzz" for numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
+
+`)
+
+numbers = [];
+
+// simple solution
+for (let num = 1; num <= 100; num++) {
+    if (num % 3 === 0 && num % 5 === 0) {
+        numbers.push('fizzbuzz');
+    } else if (num % 3 === 0) {
+        numbers.push('fizz');
+    } else if (num % 5 === 0) {
+        numbers.push('buzz');
+    } else {
+        numbers.push(num);
+    }
+}
+
+console.log(`${numbers}`);
+
+numbers = [];
+
+// smarter solution
+for (let num = 1; num <= 100; num++) {
+    let string = "";
+    if (num % 3 === 0) { string += "fizz"; }
+    if (num % 5 === 0) { string += "buzz"; }
+    numbers.push(string || num);
+}
+
+// console.log(`${numbers}`);
+
+
+
+// Chessboard
+console.log('%c Chessboard', hStyle);
+console.log(`
+
+Write a program that creates a string that represents an 8x8 grid, using newline characters to separate lines. 
+
+At each position of the grid there is either a space or a "#" character. The characters should form a chessboard.
+
+Passing this string to console.log should show something like this:
+
+ # # # #
+# # # # 
+ # # # #
+# # # # 
+ # # # #
+# # # # 
+ # # # #
+# # # #
+
+`)
+
+for (let x = 0; x <= 7; x++) {
+    let string = "";
+
+    for (let i = 0; i <= 7; i++) {
+        if (i % 2 === 0) {
+            if (x % 2 === 0) {
+                string += " ";
+            } else {
+                string += "#";
+            }
+        } else {
+            if (x % 2 === 0) {
+                string += "#";
+            } else {
+                string += " ";
+            }
+        }
+    }
+
+    console.log(string);
+}
+
+
+
+console.log(`
+When you have a program that generates this pattern, define a binding size = 8 and change the program so that it works for any size, outputting a grid of the given width and height.
+    
+`)
+
+let size = 24;
+
+for (let x = 0; x <= size - 1; x++) {
+    let string = "";
+
+    for (let i = 0; i <= size - 1; i++) {
+        if (i % 2 === 0) {
+            if (x % 2 === 0) {
+                string += " ";
+            } else {
+                string += "#";
+            }
+        } else {
+            if (x % 2 === 0) {
+                string += "#";
+            } else {
+                string += " ";
+            }
+        }
+    }
+
+    console.log(string);
+}
